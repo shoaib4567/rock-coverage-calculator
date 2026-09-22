217952630761a9b096994313c60828c5b31d196f
shoaibli <shoaibdigital.sd@gmail.com>
2026-09-22 17:04:09 +0500

    Update URL to /rock-coverage-calculator-yard with redirects and updated cluster links

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,9 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://rockcoveragecalculator.com'
+  site: 'https://rockcoveragecalculator.com',
+  redirects: {
+    '/rock-cubic-yard-calculator': '/rock-coverage-calculator-yard',
+    '/rock-coverage-calculator-yards': '/rock-coverage-calculator-yard'
+  }
 });
